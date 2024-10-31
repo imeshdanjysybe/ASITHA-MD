4d97194839203b7ca43d14f893d12762b4bae077
imeshdanjysybe <129055510+imeshdanjysybe@users.noreply.github.com>
2024-10-31 20:50:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=KrgQEJbQ#eOC6MmI5RG65Y4EMrnnoycg2s4cyxMLMqtsdcfGvUsk",
+MONGODB: process.env.MONGODB || "mongodb+srv://imeshnewhotmail:
+2WTp4IQxmGDniqJX@cluster0.uhscl.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
